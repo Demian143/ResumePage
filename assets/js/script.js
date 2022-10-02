97497df2aaffbd4b0ebacda2043fb7e6749cc682
Demian143 <patrickxavier140@gmail.com>
2022-10-02 15:15:13 -0300

    Adding switch buttons for dark mode and change the language

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-const buttonLang = document.getElementById('settings');
-
 function showTab() {
     const tab = document.getElementById('tab');
 
@@ -11,6 +9,38 @@ function showTab() {
     }
 }
 
-buttonLang.addEventListener('click', () => {
-    showTab();
-});
+const buttonLang = document.getElementById('settings');
+buttonLang.onclick = showTab;
+
+
+function switchLanguage() {
+    lang.classList.toggle('active');
+
+    language = document.getElementById('PT-EN');
+    if (language.innerHTML == 'en') {
+        language.innerHTML = 'pt';
+    }
+    else {
+        language.innerHTML = 'en';
+    }
+}
+
+const lang = document.getElementById('language');
+lang.onclick = switchLanguage;
+
+
+function swtichDarkMode() {
+    darkMode.classList.toggle('active');
+
+    dark_mode = document.getElementById('on-off');
+    if (dark_mode.innerHTML == 'off') {
+        dark_mode.innerHTML = 'on';
+    }
+    else {
+        dark_mode.innerHTML = 'off';
+    }
+}
+
+const darkMode = document.getElementById('dark-mode');
+darkMode.onclick = swtichDarkMode;
+
